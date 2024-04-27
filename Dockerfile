49f4b035a9f4b097789344667659cc29c51d3182
Arij Thabet <arijthabet01@gmail.com>
2024-04-27 19:27:45 +0100

    add: announcements module crud

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Use the official MySQL image as a base
-FROM mysql:latest
-
-# Set environment variables
-ENV MYSQL_ROOT_PASSWORD password
-ENV MYSQL_DATABASE projet_nest
-
-# Expose the MySQL port
-EXPOSE 3306
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './posts/entities/post.entity';
 import { Files } from './posts/entities/file.entity';
+import { AnnouncementModule } from './announcement/announcement.module';
+import { Announcement } from './announcement/entities/announcement.entity';
 
 @Module({
   imports: [CommonModule, PostsModule,
@@ -16,11 +16,10 @@ import { Files } from './posts/entities/file.entity';
       username: 'postgres',
       password: '0000',
       database: 'ClassroomDb',
-      entities: [Post,Files],
+      entities: [Post,Files,Announcement],
       synchronize: true,
     }),
+    AnnouncementModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
